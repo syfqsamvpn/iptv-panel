fec48eb8fa4b0013ea359ee03d9b1bd0b24dd727
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2024-03-06 20:24:45 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 clear
 apt install vnstat -y
 pip3 install pycryptodome
+pip3 install flask_cors
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/menu.sh" >/usr/bin/menu && chmod +x /usr/bin/menu
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/main.py" >/root/iptv-panel/main.py
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/system_ott.py" >/root/iptv-panel/system_ott.py
