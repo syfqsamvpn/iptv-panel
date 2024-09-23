5b0a912eb08fd3234bc75cc5870251fe687aae4c
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2024-09-23 19:20:12 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -17,14 +17,25 @@ curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/templates/
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/menu.sh" >/usr/bin/menu && chmod +x /usr/bin/menu
 curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/start_bot.sh" >/usr/bin/start_bot.sh && chmod +x /usr/bin/start_bot.sh
 #curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/ott_sam.sh" >/usr/bin/ott_sam.sh && chmod +x /usr/bin/ott_sam.sh
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/main.py" >/root/iptv-panel/main.py
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/system_ott.py" >/root/iptv-panel/system_ott.py
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/reseller.py" >/root/iptv-panel/reseller.py
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/vod.py" >/root/iptv-panel/vod.py
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/sam_enc.py" >/root/iptv-panel/sam_enc.py
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/sam_secure.py" >/root/iptv-panel/sam_secure.py
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/__init__.py" >/root/iptv-panel/pytransform/__init__.py
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/_pytransform.so" >/root/iptv-panel/pytransform/_pytransform.so
+if [ "$(grep -o 'VERSION_ID="[^"]*' /etc/os-release | grep -o '[^"]*$')" == '10' ]; then
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/main.py" >/root/iptv-panel/main.py
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/system_ott.py" >/root/iptv-panel/system_ott.py
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/reseller.py" >/root/iptv-panel/reseller.py
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/vod.py" >/root/iptv-panel/vod.py
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/sam_enc.py" >/root/iptv-panel/sam_enc.py
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/sam_secure.py" >/root/iptv-panel/sam_secure.py
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/pytransform/__init__.py" >/root/iptv-panel/pytransform/__init__.py
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/pytransform/_pytransform.so" >/root/iptv-panel/pytransform/_pytransform.so
+else
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/main.py" >/root/iptv-panel/main.py
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/system_ott.py" >/root/iptv-panel/system_ott.py
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/reseller.py" >/root/iptv-panel/reseller.py
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/vod.py" >/root/iptv-panel/vod.py
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/sam_enc.py" >/root/iptv-panel/sam_enc.py
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/sam_secure.py" >/root/iptv-panel/sam_secure.py
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/__init__.py" >/root/iptv-panel/pytransform/__init__.py
+    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/_pytransform.so" >/root/iptv-panel/pytransform/_pytransform.so
+fi
 
 if [ "$(grep -wc "REFF_STAT" "/root/iptv-panel/data.txt")" == '0' ]; then
     echo 'REFF_STAT = "on"                                                                        # Should be on/off (case sensitive)' >>"/root/iptv-panel/data.txt"
