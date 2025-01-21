92ed97d3ec70a8660e35899908fe482ed457627e
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2025-01-21 20:12:30 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,108 +1,105 @@
 #!/bin/bash
+
 clear
-apt install vnstat -y
-apt install bc -y
-pip3 install pycryptodome
-pip3 install flask_cors
-pip3 install Flask[async]
-pip3 install Flask-Limiter
-pip3 install flask_limiter
-[[ ! -d /root/iptv-panel/templates ]] && {
-    mkdir /root/iptv-panel/templates
+
+# Function to install packages if not already installed
+install_packages() {
+    for package in "$@"; do
+        if ! dpkg -s "$package" &>/dev/null; then
+            apt install -y "$package"
+        fi
+    done
 }
 
-[[ ! -d /root/iptv-panel/banned ]] && {
-    mkdir /root/iptv-panel/banned
-    touch "/root/iptv-panel/banned/banned_userid.txt"
-    touch "/root/iptv-panel/banned/banned_ip.txt"
+# Function to install Python packages
+install_python_packages() {
+    for package in "$@"; do
+        pip3 install "$package"
+    done
 }
 
-[[ ! -f "/root/iptv-panel/banned/banned_useragent.txt" ]] && {
-    touch "/root/iptv-panel/banned/banned_useragent.txt"
+# Function to create directories and files if not exist
+ensure_dir_and_files() {
+    local dir="$1"
+    shift
+    [[ ! -d "$dir" ]] && mkdir -p "$dir"
+    for file in "$@"; do
+        [[ ! -f "$dir/$file" ]] && touch "$dir/$file"
+    done
 }
 
-[[ ! -d /root/enc ]] && {
-    mkdir /root/enc
-    touch /root/enc/pytransform
+# Function to download files
+download_file() {
+    local url="$1"
+    local dest="$2"
+    curl -s "$url" -o "$dest"
 }
 
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/templates/reseller_users.html" >/root/iptv-panel/templates/reseller_users.html
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/menu.sh" >/usr/bin/menu && chmod +x /usr/bin/menu
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/start_bot.sh" >/usr/bin/start_bot.sh && chmod +x /usr/bin/start_bot.sh
-#curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/ott_sam.sh" >/usr/bin/ott_sam.sh && chmod +x /usr/bin/ott_sam.sh
-if [ "$(grep -o 'VERSION_ID="[^"]*' /etc/os-release | grep -o '[^"]*$')" == '10' ]; then
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/main.py" >/root/iptv-panel/main.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/system_ott.py" >/root/iptv-panel/system_ott.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/reseller.py" >/root/iptv-panel/reseller.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/vod.py" >/root/iptv-panel/vod.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/sam_enc.py" >/root/iptv-panel/sam_enc.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/sam_secure.py" >/root/iptv-panel/sam_secure.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/pytransform/__init__.py" >/root/iptv-panel/pytransform/__init__.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv/main/deb10/pytransform/_pytransform.so" >/root/iptv-panel/pytransform/_pytransform.so
+# Install necessary packages
+install_packages vnstat bc
 
-    #ENC
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/enc_deb10/main.py" >/root/enc/main.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/enc_deb10/pytransform/__init__.py" >/root/enc/pytransform/__init__.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/enc_deb10/pytransform/_pytransform.so" >/root/enc/pytransform/_pytransform.so
+# Install necessary Python packages
+install_python_packages pycryptodome flask_cors Flask[async] Flask-Limiter flask_limiter
+
+# Ensure required directories and files exist
+ensure_dir_and_files "/root/iptv-panel/templates"
+ensure_dir_and_files "/root/iptv-panel/module"
+ensure_dir_and_files "/root/iptv-panel/banned" "banned_userid.txt" "banned_ip.txt" "banned_useragent.txt"
+ensure_dir_and_files "/root/enc" "pytransform"
+
+# Download specific files
+download_file "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/templates/reseller_users.html" "/root/iptv-panel/templates/reseller_users.html"
+download_file "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/menu.sh" "/usr/bin/menu" && chmod +x /usr/bin/menu
+download_file "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/start_bot.sh" "/usr/bin/start_bot.sh" && chmod +x /usr/bin/start_bot.sh
+
+# Check Debian version and download specific files accordingly
+if [[ "$(grep -o 'VERSION_ID="[^"]*' /etc/os-release | grep -o '[^"]*$')" == "10" ]]; then
+    echo "Please Upgrade to DEBIAN 11"
+    exit 0
 else
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/main.py" >/root/iptv-panel/main.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/system_ott.py" >/root/iptv-panel/system_ott.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/reseller.py" >/root/iptv-panel/reseller.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/vod.py" >/root/iptv-panel/vod.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/sam_enc.py" >/root/iptv-panel/sam_enc.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/sam_secure.py" >/root/iptv-panel/sam_secure.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/__init__.py" >/root/iptv-panel/pytransform/__init__.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/_pytransform.so" >/root/iptv-panel/pytransform/_pytransform.so
-
-    # ENC
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/enc_deb11/main.py" >/root/enc/main.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/enc_deb11/pytransform/__init__.py" >/root/enc/pytransform/__init__.py
-    curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/enc_deb11/pytransform/_pytransform.so" >/root/enc/pytransform/_pytransform.so
-fi
-curl -s "http://api.samhub.my.id/enc/guardian.sh" >/root/enc/guardian.sh && chmod +x /root/enc/guardian.sh
-
-if [ "$(grep -wc "REFF_STAT" "/root/iptv-panel/data.txt")" == '0' ]; then
-    echo 'REFF_STAT = "on"                                                                        # Should be on/off (case sensitive)' >>"/root/iptv-panel/data.txt"
-fi
-if [ "$(grep -wc "TELEGRAM_ADMIN_ID" "/root/iptv-panel/data.txt")" == '0' ]; then
-    echo 'TELEGRAM_ADMIN_ID = "input_id_tele"                                                     # telegram admin id' >>"/root/iptv-panel/data.txt"
-fi
-if [ "$(grep -wc "VOD_FILE" "/root/iptv-panel/data.txt")" == '0' ]; then
-    echo 'VOD_FILE = "vod.m3u"                                                                    # change this to change vod' >>"/root/iptv-panel/data.txt"
-    touch "/root/iptv-panel/vod.m3u"
-fi
-if [ "$(grep -wc "PASSWORD_SEC" "/root/iptv-panel/data.txt")" == '0' ]; then
-    echo 'PASSWORD_SEC = "on"                                                                     # Should be on/off (case sensitive)' >>"/root/iptv-panel/data.txt"
+    BASE_URL="https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main"
+    DEST_DIR="/root/iptv-panel"
+    FILES=(
+        "pytransform/__init__.py"
+        "pytransform/_pytransform.so"
+        "main.py"
+        "reseller.py"
+        "sam_secure.py"
+        "vod.py"
+        "module/__init__.py"
+        "module/system_ott.py"
+        "module/sam_enc.py"
+    )
+    for file in "${FILES[@]}"; do
+        mkdir -p "$DEST_DIR/$(dirname "$file")" # Ensure target directory exists
+        download_file "$BASE_URL/$file" "$DEST_DIR/$file"
+    done
 fi
 
-if [ "$(grep -wc "OFFLINE_REDIRECT" "/root/iptv-panel/data.txt")" == '0' ]; then
-    echo 'OFFLINE_REDIRECT = "https://d25tgymtnqzu8s.cloudfront.net/smil:tv1/playlist.m3u8?id=1"  # Should be on/off (case sensitive)' >>"/root/iptv-panel/data.txt"
-fi
+# Download guardian script
+download_file "http://api.samhub.my.id/enc/guardian.sh" "/root/enc/guardian.sh" && chmod +x /root/enc/guardian.sh
 
-if [ ! -d "/root/iptv-panel/secure/" ]; then
-    mkdir -p "/root/iptv-panel/secure/"
-fi
+# Ensure data.txt has required configurations
+DATA_FILE="/root/iptv-panel/data.txt"
+declare -A CONFIGS=(
+    ["REFF_STAT"]='REFF_STAT = "on"                                                                        # Should be on/off (case sensitive)'
+    ["TELEGRAM_ADMIN_ID"]='TELEGRAM_ADMIN_ID = "input_id_tele"                                             # telegram admin id'
+    ["VOD_FILE"]='VOD_FILE = "vod.m3u"                                                                    # change this to change vod'
+    ["PASSWORD_SEC"]='PASSWORD_SEC = "on"                                                                 # Should be on/off (case sensitive)'
+    ["OFFLINE_REDIRECT"]='OFFLINE_REDIRECT = "https://d25tgymtnqzu8s.cloudfront.net/smil:tv1/playlist.m3u8?id=1"  # Redirect link'
+    ["FREEMIUM_FILE"]='FREEMIUM_FILE = "freemium.m3u"                                                     # change this to change freemium playlist'
+    ["SHORT_LINK"]='SHORT_LINK = "off"                                                                    # Should be off/short_domain (case sensitive)'
+    ["VOD_SHORT"]='VOD_SHORT = "vod_short.json"                                                           # vod links'
+    ["BUYOTT_FILE"]='BUYOTT_FILE = "buy_ott.m3u"                                                          # change this to change buy ott playlist'
+)
+for key in "${!CONFIGS[@]}"; do
+    if ! grep -qw "$key" "$DATA_FILE"; then
+        echo "${CONFIGS[$key]}" >>"$DATA_FILE"
+    fi
+done
 
-if [ ! -d "/root/iptv-panel/static/var/" ]; then
-    mkdir -p "/root/iptv-panel/static/var/"
-fi
-
-if [ ! -f "/root/iptv-panel/expired.json" ]; then
-    touch "/root/iptv-panel/expired.json"
-    echo 'EXPIRED_DATA = "expired.json"                                                           # Expired data' >>"/root/iptv-panel/data.txt"
-fi
-if [ "$(grep -wc "FREEMIUM_FILE" "/root/iptv-panel/data.txt")" == '0' ]; then
-    echo 'FREEMIUM_FILE = "freemium.m3u"                                                          # change this to change freemium playlist' >>"/root/iptv-panel/data.txt"
-fi
-
-if [ "$(grep -wc "SHORT_LINK =" "/root/iptv-panel/data.txt")" == '0' ]; then
-    echo 'SHORT_LINK = "off"                                                                      # Should be off/short_domain (case sensitive)' >>"/root/iptv-panel/data.txt"
-fi
-
-if [ "$(grep -wc "VOD_SHORT" "/root/iptv-panel/data.txt")" == '0' ]; then
-    echo 'VOD_SHORT = "vod_short.json"                                                            # vod links' >>"/root/iptv-panel/data.txt"
-fi
-
-if [ "$(grep -wc "BUYOTT_FILE" "/root/iptv-panel/data.txt")" == '0' ]; then
-    echo 'BUYOTT_FILE = "buy_ott.m3u"                                                             # change this to change buy ott playlist' >>"/root/iptv-panel/data.txt"
-fi
+# Additional directories and files
+ensure_dir_and_files "/root/iptv-panel/secure"
+ensure_dir_and_files "/root/iptv-panel/static/var"
+[[ ! -f "/root/iptv-panel/expired.json" ]] && touch "/root/iptv-panel/expired.json" && echo 'EXPIRED_DATA = "expired.json" # Expired data' >>"$DATA_FILE"
+[[ ! -f "/root/iptv-panel/vod.m3u" ]] && touch "/root/iptv-panel/vod.m3u"
